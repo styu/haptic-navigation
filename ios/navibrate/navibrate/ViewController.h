9398ae627e9c85db48fddb798a8cf8d0864ffe5a
Stephanie Yu <styu314@gmail.com>
2014-10-04 15:12:11 -0400

    initial iphone app

---
+++ ios/navibrate/navibrate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  navibrate
+//
+//  Created by Stephanie Yu on 10/4/14.
+//  Copyright (c) 2014 Stephanie Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios/navibrate/navibrateTests/navibrateTests.m
@@ -0,0 +1,40 @@
+//
+//  navibrateTests.m
+//  navibrateTests
+//
+//  Created by Stephanie Yu on 10/4/14.
+//  Copyright (c) 2014 Stephanie Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface navibrateTests : XCTestCase
+
+@end
+
+@implementation navibrateTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
